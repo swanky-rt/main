9d58681ae67a04ecadeb87c4ccc0f3c0836bc0cd
rithikafjohnson <rithikajohnson@gmail.com>
2025-03-05 13:40:39 -0500

    Latest_changes

--- app/src/main/java/project_645/PageImpl.java
+++ app/src/main/java/project_645/PageImpl.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 
 public class PageImpl implements Page, Serializable {
-    static final int PAGE_SIZE = 4096; // 4kb
+    static final int PAGE_SIZE = 4096;
     static final int ROW_SIZE = 39; //column 1 is 9 char, and column 2 is 30 char
     static final int MAX_TUPLES = PAGE_SIZE/ROW_SIZE;
     private Row[] rowList = new Row[MAX_TUPLES];
